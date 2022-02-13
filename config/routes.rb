1f1c0d3c3fcddaf1970e52826fb856e5f4728b87
adimin1 <matheuscarpesmc2@gmail.com>
2022-02-12 21:12:39 -0300

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :vacancies do 
     resources :applicants, only: %i[index]
   end
-  
   resources :vacancies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
