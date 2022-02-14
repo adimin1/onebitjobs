f07e37adfb62c804bd1d71089c9af2c0aa7657a5
adimin1 <matheuscarpesmc2@gmail.com>
2022-02-14 18:42:02 -0300

    last update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :companies
   get '/vacancies/all' => 'vacancies#all'
-    authenticated :company do
-      root to: 'vacancies#index', as: :company_root
-    end
-    unauthenticated :company do
-      root to: 'vacancies#all', as: 'unauthenticated_root'
-    end
+  authenticated :company do
+    root to: 'vacancies#index', as: :company_root
+  end
+  unauthenticated :company do
+    root to: 'vacancies#all', as: 'unauthenticated_root'
+  end
   resources :applicants, only: :create
-  resources :vacancies do 
+  resources :vacancies do
     resources :applicants, only: %i[index]
   end
   resources :vacancies
